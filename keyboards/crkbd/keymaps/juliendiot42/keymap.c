30e22e8771c174eb7339222acbc4fca0809be440
Julien Diot <juliendiot42@gmail.com>
2021-09-12 17:51:43 +0900

    add my removed keyboard

---
+++ keyboards/crkbd/keymaps/juliendiot42/keymap.c
@@ -0,0 +1,214 @@
+/*
+Copyright 2019 @foostan
+Copyright 2020 Drashna Jaelre <@drashna>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include QMK_KEYBOARD_H
+
+#define _LAYER0 0
+#define _LAYER1 1
+#define _LAYER2 2
+#define _LAYER3 3
+#define _LAYER4 4
+
+enum custom_keycodes {
+    LAYER0 = SAFE_RANGE,
+    LAYER1,
+    LAYER2,
+    LAYER3,
+    LAYER4,
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+ [_LAYER0] = LAYOUT(KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LALT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_DEL, KC_LGUI, LT(1,KC_SPC), KC_LSFT, KC_LCTL, LT(2,KC_ENT), KC_RALT),
+
+[_LAYER1] = LAYOUT(KC_TAB, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC, KC_ESC, KC_PGUP, KC_NO, KC_WH_D, KC_WH_U, KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_NO, KC_LALT, KC_PGDN, KC_CUT, KC_COPY, KC_PSTE, KC_NO, KC_NO, KC_HOME, KC_END, KC_NO, KC_NO, KC_DEL, KC_LGUI, KC_TRNS, KC_LSFT, KC_LCTL, MO(3), KC_RALT),
+
+[_LAYER2] = LAYOUT(KC_TAB, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, KC_ESC, KC_NO, KC_TILD, KC_LCBR, KC_LPRN, KC_LBRC, KC_RBRC, KC_RPRN, KC_RCBR, KC_BSLS, KC_GRV, KC_NO, KC_LALT, KC_NO, KC_NO, KC_NO, KC_EQL, KC_MINS, KC_PLUS, KC_UNDS, KC_PIPE, KC_NO, KC_NO, KC_DEL, KC_LGUI, MO(3), KC_LCTL, KC_RCTL, KC_TRNS, KC_RALT),
+
+[_LAYER3] = LAYOUT(KC_F14, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_PSCR, KC_NO, KC_NO, KC_NO, KC_VOLD, KC_VOLU, KC_F13, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_MUTE, KC_MPLY, KC_LCTL, KC_LSFT, KC_F11, KC_F12, KC_NO, KC_HANJ, KC_BTN1, KC_BTN3, KC_BTN2, KC_NO, KC_RSFT, KC_RCTL, KC_NO, KC_NO, KC_CAPS, RESET, KC_NO, TO(4)),
+
+[_LAYER4] = LAYOUT(RGB_SPI, RGB_VAI, RGB_SAI, RGB_HUI, RGB_MOD, RGB_TOG, BL_TOGG, BL_STEP, BL_BRTG, BL_INC, KC_NO, KC_NO, RGB_SPD, RGB_VAD, RGB_SAD, RGB_HUD, RGB_RMOD, KC_NO, KC_NO, KC_NO, KC_NO, BL_DEC, KC_NO, KC_NO, RGB_M_G, RGB_M_SN, RGB_M_SW, RGB_M_R, RGB_M_B, RGB_M_P, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, TO(0)) 
+
+};
+
+// const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+//   [0] = LAYOUT_split_3x6_3(
+//   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+//        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
+//   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+//       KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+//   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+//       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ESC,
+//   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+//                                           KC_LGUI,   MO(1),  KC_SPC,     KC_ENT,   MO(2), KC_RALT
+//                                       //`--------------------------'  `--------------------------'
+
+//   ),
+
+//   [1] = LAYOUT_split_3x6_3(
+//   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+//        KC_TAB,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,
+//   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+//       KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX, XXXXXXX,
+//   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+//       KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+//   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+//                                           KC_LGUI, _______,  KC_SPC,     KC_ENT,   MO(3), KC_RALT
+//                                       //`--------------------------'  `--------------------------'
+//   ),
+
+//   [2] = LAYOUT_split_3x6_3(
+//   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+//        KC_TAB, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+//   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+//       KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,  KC_GRV,
+//   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+//       KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
+//   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+//                                           KC_LGUI,   MO(3),  KC_SPC,     KC_ENT, _______, KC_RALT
+//                                       //`--------------------------'  `--------------------------'
+//   ),
+
+//   [3] = LAYOUT_split_3x6_3(
+//   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+//         RESET, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+//   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+//       RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+//   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+//       RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+//   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+//                                           KC_LGUI, _______,  KC_SPC,     KC_ENT, _______, KC_RALT
+//                                       //`--------------------------'  `--------------------------'
+//   )
+// };
+
+#ifdef OLED_ENABLE
+oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+    if (!is_keyboard_master()) {
+        return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
+    }
+    return rotation;
+}
+
+void oled_render_layer_state(void) {
+    oled_write_P(PSTR("Layer: "), false);
+    switch (get_highest_layer(layer_state)) {
+        case _LAYER0:
+            oled_write_P(PSTR("abc\n"), false);
+            break;
+        case _LAYER1:
+            oled_write_P(PSTR("123 ->\n"), false);
+            break;
+        case _LAYER2:
+            oled_write_P(PSTR("!@#$\n"), false);
+            break;
+       case _LAYER3:
+            oled_write_P(PSTR("Spec\n"), false);
+            break;
+      case _LAYER4:
+            oled_write_P(PSTR("RGB\n"), false);
+            break;
+        default:
+            // Or use the write_ln shortcut over adding '\n' to the end of your string
+            oled_write_ln_P(PSTR("Undefined"), false);
+    }
+    // Host Keyboard LED Status
+    //led_t led_state = host_keyboard_led_state();
+    //oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
+    //oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
+    //oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
+}
+
+void render_bootmagic_status(bool status) {
+    /* Show Ctrl-Gui Swap options */
+     /*static const char PROGMEM logo[][2][3] = {
+        {{0x97, 0x98, 0}, {0xb7, 0xb8, 0}},
+        {{0x95, 0x96, 0}, {0xb5, 0xb6, 0}},
+    };
+    if (status) {
+        oled_write_ln_P(logo[0][0], false);
+        oled_write_ln_P(logo[0][1], false);
+    } else {
+        oled_write_ln_P(logo[1][0], false);
+        oled_write_ln_P(logo[1][1], false);
+    }*/
+    oled_write_P(PSTR("BOOTMAGIC"), false);
+}
+
+void oled_render_logo(void) {
+    static const char PROGMEM crkbd_logo[] = {0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94, 0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4, 0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00};
+    oled_write_P(crkbd_logo, false);
+}
+
+void oled_task_user(void) {
+    if (is_keyboard_master()) {
+        oled_render_layer_state();
+        // oled_render_keylog();
+    } else {
+        oled_render_logo();
+    }
+}
+
+#endif  // OLED_ENABLE
+
+// // Light LEDs 6 to 9 and 12 to 15 red when caps lock is active. Hard to ignore!
+// const rgblight_segment_t PROGMEM my_capslock_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+//     {6, 4, HSV_GREEN},       // Light 4 LEDs, starting with LED 6
+//     {12, 4, HSV_GREEN}       // Light 4 LEDs, starting with LED 12
+// );
+// // Light LEDs 9 & 10 in cyan when keyboard layer 1 is active
+// const rgblight_segment_t PROGMEM my_layer1_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+//     {9, 2, HSV_CYAN}
+// );
+// // Light LEDs 11 & 12 in purple when keyboard layer 2 is active
+// const rgblight_segment_t PROGMEM my_layer2_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+//     {11, 2, HSV_PURPLE}
+// );
+// // Light LEDs 13 & 14 in green when keyboard layer 3 is active
+// const rgblight_segment_t PROGMEM my_layer3_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+//     {13, 2, HSV_GREEN}
+// );
+
+// // Now define the array of layers. Later layers take precedence
+// const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
+//     my_capslock_layer,
+//     my_layer1_layer,    // Overrides caps lock layer
+//     my_layer2_layer,    // Overrides other layers
+//     my_layer3_layer     // Overrides other layers
+// );
+
+// void keyboard_post_init_user(void) {
+//     // Enable the LED layers
+//     rgblight_layers = my_rgb_layers;
+// }
+
+// bool led_update_user(led_t led_state) {
+//     rgblight_set_layer_state(0, led_state.caps_lock);
+//     return true;
+// }
+
+// layer_state_t default_layer_state_set_user(layer_state_t state) {
+//     rgblight_set_layer_state(1, layer_state_cmp(state, _LAYER1));
+//     return state;
+// }
+
+// layer_state_t layer_state_set_user(layer_state_t state) {
+//     rgblight_set_layer_state(2, layer_state_cmp(state, _LAYER2));
+//     rgblight_set_layer_state(3, layer_state_cmp(state, _LAYER3));
+//     return state;
+// }
---
+++ keyboards/crkbd/keymaps/juliendiot42/rules.mk
@@ -0,0 +1,6 @@
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306
+RGBLIGHT_ENABLE=yes
+SLEEP_LED_ENABLE=yes
+MOUSEKEY_ENABLE=no
+this_dir=keyboards/crkbd/keymaps/juliendiot42
