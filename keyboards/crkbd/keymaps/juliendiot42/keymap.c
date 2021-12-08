e5c5e589485a73850423d809f040a6517e6be03e
Julien Diot <juliendiot42@gmail.com>
2021-12-08 13:37:39 +0900

    format keys definition

--- keyboards/crkbd/keymaps/juliendiot42/keymap.c
+++ keyboards/crkbd/keymaps/juliendiot42/keymap.c
@@ -44,15 +44,35 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
- [_LAYER0] = LAYOUT(KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LALT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_DEL, KC_LGUI, LT(1,KC_SPC), KC_LSFT, KC_LCTL, LT(2,KC_ENT), TD(TD_RALT_APP)),
+ [_LAYER0] = LAYOUT(
+KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,       KC_BSPC,
+KC_ESC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,            KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,    KC_QUOT,
+KC_LALT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,    KC_DEL,
+                        KC_LGUI, LT(1,KC_SPC), KC_LSFT,         KC_LCTL, LT(2,KC_ENT), TD(TD_RALT_APP)),
 
-[_LAYER1] = LAYOUT(KC_TAB, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC, KC_ESC, KC_PGUP, KC_NO, KC_WH_D, KC_WH_U, KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_NO, KC_LALT, KC_PGDN, KC_CUT, KC_COPY, KC_PSTE, KC_NO, KC_NO, KC_HOME, KC_END, KC_DOT, KC_NO, KC_DEL, KC_LGUI, KC_TRNS, KC_LSFT, KC_LCTL, MO(3), KC_RALT),
+[_LAYER1] = LAYOUT(
+KC_TAB,    KC_1,     KC_2,    KC_3,    KC_4,    KC_5,            KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_BSPC,
+KC_ESC,    KC_PGUP,  KC_NO,   KC_WH_D, KC_WH_U, KC_NO,           KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_NO,    KC_NO,
+KC_LALT,   KC_PGDN,  KC_CUT,  KC_COPY, KC_PSTE, KC_NO,           KC_NO,    KC_HOME,  KC_END,   KC_DOT,   KC_NO,    KC_DEL,
+                        KC_LGUI, KC_TRNS, KC_LSFT,               KC_LCTL, MO(3),    KC_RALT),
 
-[_LAYER2] = LAYOUT(KC_TAB, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, KC_ESC, KC_NO, KC_TILD, KC_LCBR, KC_LPRN, KC_LBRC, KC_RBRC, KC_RPRN, KC_RCBR, KC_BSLS, KC_GRV, KC_NO, KC_LALT, KC_NO, KC_NO, KC_NO, KC_EQL, KC_MINS, KC_PLUS, KC_UNDS, KC_PIPE, KC_NO, KC_NO, KC_DEL, KC_LGUI, MO(3), KC_LCTL, KC_RCTL, KC_TRNS, KC_RALT),
+[_LAYER2] = LAYOUT(
+KC_TAB,    KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,    KC_PERC,            KC_CIRC,    KC_AMPR,    KC_ASTR,    KC_LPRN,    KC_RPRN,    KC_BSPC,
+KC_ESC,    KC_NO,    KC_TILD,  KC_LCBR,  KC_LPRN,   KC_LBRC,            KC_RBRC,    KC_RPRN,    KC_RCBR,    KC_BSLS,    KC_GRV,     KC_NO,
+KC_LALT,   KC_NO,    KC_NO,    KC_NO,    KC_EQL,    KC_MINS,            KC_PLUS,    KC_UNDS,    KC_PIPE,    KC_NO,      KC_NO,      KC_DEL,
+                              KC_LGUI,    MO(3),    KC_LCTL,           KC_RCTL,    KC_TRNS,    KC_RALT),
 
-[_LAYER3] = LAYOUT(KC_F14, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_PSCR, KC_NO, KC_NO, KC_NO, KC_VOLD, KC_VOLU, KC_F13, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_MUTE, KC_MPLY, KC_LCTL, KC_LSFT, KC_F11, KC_F12, KC_NO, KC_HENK, KC_BTN1, KC_BTN3, KC_BTN2, KC_NO, KC_RSFT, KC_RCTL, KC_LALT, KC_NO, KC_CAPS, RESET, KC_NO, TO(4)),
+[_LAYER3] = LAYOUT(
+KC_F14,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,              KC_PSCR,    KC_NO,      KC_NO,    KC_NO,    KC_VOLD,    KC_VOLU,
+KC_F13,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,             KC_MS_L,    KC_MS_D,    KC_MS_U,  KC_MS_R,  KC_MUTE,    KC_MPLY,
+KC_LCTL,   KC_LSFT,  KC_F11,   KC_F12,   KC_NO,    KC_HENK,            KC_BTN1,    KC_BTN3,    KC_BTN2,  KC_NO,    KC_RSFT,    KC_RCTL,
+                              KC_LALT,    KC_NO,    KC_CAPS,           RESET,    KC_NO,    TO(4)),
 
-[_LAYER4] = LAYOUT(RGB_SPI, RGB_VAI, RGB_SAI, RGB_HUI, RGB_MOD, RGB_TOG, BL_TOGG, BL_STEP, BL_BRTG, BL_INC, KC_NO, KC_NO, RGB_SPD, RGB_VAD, RGB_SAD, RGB_HUD, RGB_RMOD, KC_NO, KC_NO, KC_NO, KC_NO, BL_DEC, KC_NO, KC_NO, RGB_M_G, RGB_M_SN, RGB_M_SW, RGB_M_R, RGB_M_B, RGB_M_P, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, TO(0)) 
+[_LAYER4] = LAYOUT(
+RGB_SPI,    RGB_VAI,    RGB_SAI,    RGB_HUI,    RGB_MOD,    RGB_TOG,               BL_TOGG,    BL_STEP,    BL_BRTG,    BL_INC,    KC_NO,    KC_NO,
+RGB_SPD,    RGB_VAD,    RGB_SAD,    RGB_HUD,    RGB_RMOD,   KC_NO,                 KC_NO,      KC_NO,      KC_NO,      BL_DEC,    KC_NO,    KC_NO,
+RGB_M_G,    RGB_M_SN,   RGB_M_SW,   RGB_M_R,    RGB_M_B,    RGB_M_P,               KC_NO,      KC_NO,      KC_NO,      KC_NO,     KC_NO,    KC_NO,
+                                         KC_NO,    KC_NO,    KC_NO,                 KC_NO,    KC_NO,    TO(0)) 
 
 };
 
